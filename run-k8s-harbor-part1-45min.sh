591b22533a21584a951c3fe1e81c27ee9b8e4596
Petr Ruzicka <petr.ruzicka@gmail.com>
2019-10-29 14:17:22 +0100

    Adding new scripts for 45min presentation

---
+++ run-k8s-harbor-part1-45min.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# apt-get update -qq && apt-get install -qq -y curl git > /dev/null
+# cd /var/tmp/
+
+# export LETSENCRYPT_ENVIRONMENT="production"  # Use with care - Let's Encrypt will generate real certificates
+# export MY_DOMAIN="mylabs.dev"
+# ./run-k8s-harbor-part1.sh
+
+[ ! -d .git ] && git clone --quiet https://github.com/ruzickap/k8s-harbor && cd k8s-harbor
+
+grep mylabs.dev /etc/hosts
+
+sed docs/part-0{1..5}/README.md \
+  -e '/^## Configure AWS/,/^Create policy allowing the cert-manager to change Route 53 settings./d' \
+| \
+sed -n '/^```bash.*/,/^```$/p' \
+| \
+sed '/^```/d' \
+> README.sh
+
+if [ "$#" -eq 0 ]; then
+  source README.sh
+else
+  cat README.sh
+fi
---
+++ run-k8s-harbor-part2-45min.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/env bash
+
+################################################
+# include the magic
+################################################
+test -f ./demo-magic.sh || curl --silent https://raw.githubusercontent.com/paxtonhare/demo-magic/master/demo-magic.sh > demo-magic.sh
+. ./demo-magic.sh
+
+################################################
+# Configure the options
+################################################
+
+#
+# speed at which to simulate typing. bigger num = faster
+#
+TYPE_SPEED=60
+
+# Uncomment to run non-interactively
+export PROMPT_TIMEOUT=0
+
+# No wait
+export NO_WAIT=false
+
+#
+# custom prompt
+#
+# see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
+#
+#DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
+DEMO_PROMPT="${GREEN}➜ ${CYAN}$ "
+
+
+sed docs/part-0{6..8}/README.md \
+  -e '/^## Upload Helm Chart using CLI/,/^## Upload signed Helm Chart using CLI/d' \
+  -e '/^## Signed container image/,/^## Vulnerability scan/d' \
+| \
+sed -n '/^```bash/,/^```$/p;/^-----$/p' \
+| \
+sed \
+  -e 's/^-----$/\
+p  ""\
+p  "################################################################################################### Press <ENTER> to continue"\
+wait\
+/' \
+  -e 's/^```bash.*/\
+pe '"'"'/' \
+  -e 's/^```$/'"'"'/' \
+> README.sh
+
+if [ "$#" -eq 0 ]; then
+
+  ### Please run these commands before running the script
+
+  # mkdir /var/tmp/test && cd /var/tmp/test
+  # git clone --quiet https://github.com/ruzickap/k8s-harbor && cd k8s-harbor
+
+  export LETSENCRYPT_ENVIRONMENT=${LETSENCRYPT_ENVIRONMENT:-staging}
+  # export LETSENCRYPT_ENVIRONMENT="production" # Use with care - Let's Encrypt will generate real certificates
+  # ./run-k8s-harbor-part2.sh
+
+  export MY_DOMAIN="mylabs.dev"
+  export EKS_CERT_MANAGER_ROUTE53_AWS_ACCESS_KEY_ID=$(awk -F\" "/AccessKeyId/ { print \$4 }" $HOME/.aws/${USER}-eks-cert-manager-route53-${MY_DOMAIN})
+  export EKS_CERT_MANAGER_ROUTE53_AWS_SECRET_ACCESS_KEY=$(awk -F\" "/SecretAccessKey/ { print \$4 }" $HOME/.aws/${USER}-eks-cert-manager-route53-${MY_DOMAIN})
+  eksctl utils write-kubeconfig --kubeconfig kubeconfig.conf --name=${USER}-k8s-harbor
+  echo -e "\n${MY_DOMAIN} | ${EKS_CERT_MANAGER_ROUTE53_AWS_ACCESS_KEY_ID} | ${EKS_CERT_MANAGER_ROUTE53_AWS_SECRET_ACCESS_KEY}\n`kubectl --kubeconfig=./kubeconfig.conf cluster-info`"
+
+  if [ -z ${EKS_CERT_MANAGER_ROUTE53_AWS_ACCESS_KEY_ID} ] || [ -z ${EKS_CERT_MANAGER_ROUTE53_AWS_SECRET_ACCESS_KEY} ]; then
+    echo -e "\n*** One of the mandatory variables 'EKS_CERT_MANAGER_ROUTE53_AWS_ACCESS_KEY_ID' or 'EKS_CERT_MANAGER_ROUTE53_AWS_SECRET_ACCESS_KEY' is not set !!\n";
+    exit 1
+  fi
+
+  cat << \EOF
+*** Wait until Clair Vulnerability database will be fully updated
+export KUBECONFIG=$PWD/kubeconfig.conf
+CLAIR_POD=$(kubectl get pods -l "app=harbor,component=clair" -n harbor-system -o jsonpath="{.items[0].metadata.name}")
+kubectl logs -n harbor-system ${CLAIR_POD}
+EOF
+  export KUBECONFIG=$PWD/kubeconfig.conf
+  CLAIR_POD=$(kubectl get pods -l "app=harbor,component=clair" -n harbor-system -o jsonpath="{.items[0].metadata.name}")
+  COUNT=0
+  #while ! kubectl logs -n harbor-system ${CLAIR_POD} | grep "update finished"; do COUNT=$((COUNT+1)); echo -n "${COUNT} "; sleep 10; done
+
+  set -eux
+  ansible localhost -m wait_for -a "port=5986 host=winad01.${MY_DOMAIN}"
+
+  kubectl get secrets ingress-cert-${LETSENCRYPT_ENVIRONMENT} -n harbor-system
+
+  if [ ${LETSENCRYPT_ENVIRONMENT} = "staging" ]; then
+    sudo mkdir -pv /etc/docker/certs.d/harbor.${MY_DOMAIN}/
+    CA_CERT=$(kubectl get secrets ingress-cert-staging -n cert-manager -o jsonpath="{.data.ca\.crt}")
+    [ "${CA_CERT}" != "<nil>" ] && echo ${CA_CERT} | base64 -d > ca.crt
+    test -s ca.crt || wget -q https://letsencrypt.org/certs/fakelerootx1.pem -O ca.crt
+    sudo cp ca.crt /etc/docker/certs.d/harbor.${MY_DOMAIN}/ca.crt
+    export SSL_CERT_FILE=$PWD/ca.crt
+    for EXTERNAL_IP in $(kubectl get nodes --output=jsonpath="{.items[*].status.addresses[?(@.type==\"ExternalIP\")].address}"); do
+      ssh -q -o StrictHostKeyChecking=no -l ec2-user ${EXTERNAL_IP} \
+        "sudo mkdir -p /etc/docker/certs.d/harbor.${MY_DOMAIN}/ && sudo wget -q https://letsencrypt.org/certs/fakelerootx1.pem -O /etc/docker/certs.d/harbor.${MY_DOMAIN}/ca.crt"
+    done
+    echo "*** Done"
+  fi
+
+  cd tmp
+
+  awk "/${MY_DOMAIN}/" /etc/hosts
+  set +eux
+
+  echo -e "\n\n*** Press ENTER to start\n"
+  read A
+
+  # hide the evidence
+  clear
+  source ../README.sh
+else
+  cat README.sh
+fi
